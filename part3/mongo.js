8fc2a09a380845c56d437c01128c9b549487f81a
Deep <deepatel6128@gmail.com>
2024-11-21 15:35:36 -0500

    mongo changed

--- part3/mongo.js
+++ part3/mongo.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
 const password = process.argv[2];
-
 const url = `mongodb+srv://deepPatel:${password}@finding.upxe2.mongodb.net/?retryWrites=true&w=majority&appName=Finding`;
-
 mongoose.set("strictQuery", false);
 
-mongoose.connect(url);
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("Connection Successful");
+  })
+  .catch((e) => {
+    console.log("Unsuccessful connection", e.message);
+  });
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -15,7 +20,7 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model("Person", personSchema);
 
-if (process.argv[3]) {
+if (process.argv[3] && process.argv[4]) {
   const person = new Person({
     name: process.argv[3],
     number: process.argv[4],
@@ -25,6 +30,9 @@ if (process.argv[3]) {
     console.log(`added ${res.name} ${res.number} to phonebook`);
     mongoose.connection.close();
   });
+} else if (process.argv[3]) {
+  console.log("Please provide both name and number");
+  mongoose.connection.close();
 } else {
   try {
     Person.find().then((res) => {
